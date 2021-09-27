5f1b6d91272f3ca0007aaca1fcd106b404182f82
0woogie <80441481+0woogie@users.noreply.github.com>
2021-09-28 00:10:57 +0900

    Create 11_15.py

---
+++ 11_15.py
@@ -0,0 +1,28 @@
+#11번
+f = input()
+f = float(f)
+print(f)
+
+#12번
+a = input() 
+b = input()
+print(a)
+print(b)
+
+#13번
+x = input() 
+y = input()
+print(y)
+print(x)
+
+#14번
+f = input()
+f = float(f)
+print(f)
+print(f)
+print(f)
+
+#15번
+a, b = input().split()
+print(a)
+print(b)
