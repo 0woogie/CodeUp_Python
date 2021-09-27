58448fd79f118688ae05ffae84ffaf9fb598f3af
0woogie <80441481+0woogie@users.noreply.github.com>
2021-09-27 23:16:45 +0900

    Create 06_10.py

---
+++ 06_10.py
@@ -0,0 +1,17 @@
+#6번
+print("\"!@#$%^&*()\'")
+
+#7번
+print("\"C:\\Download\\\'hello\'.py\"")
+
+#8번
+print("print(\"Hello\\nWorld\")")
+
+#9번
+c = input()
+print(c)
+
+#10번
+n = input()
+n = int(n)
+print(n)
