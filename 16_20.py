3c330e89fd74b10a2c98cf9d52b1fe9858fee0c6
0woogie <80441481+0woogie@users.noreply.github.com>
2021-09-29 01:22:09 +0900

    Create 16_20.py

---
+++ 16_20.py
@@ -0,0 +1,19 @@
+#16번
+c1, c2 = input().split()
+print(c2,c1)
+
+#17번
+s = input()
+print(s, s, s)
+
+#18번
+a, b = input().split(':')
+print(a, b, sep=':')
+
+#19번
+y, m, d = input().split('.')
+print(d, m, y, sep='-')
+
+#20번
+a, b = input().split('-')
+print(a, b, sep='')
