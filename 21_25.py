e723c6085f711a69f1b2a24e90b1184ce832a705
0woogie <80441481+0woogie@users.noreply.github.com>
2021-09-30 19:10:01 +0900

    Create 21_25.py

---
+++ 21_25.py
@@ -0,0 +1,25 @@
+#21번
+s = input()
+print(s[0])
+print(s[1])
+print(s[2])
+print(s[3])
+print(s[4])
+
+#22번
+s = input()
+print(s[0:2], s[2:4], s[4:6])
+
+#23번
+h, m, s = input().split(':')
+print(m)
+
+#24번
+w1, w2 = input().split()
+s = w1 + w2
+print(s)
+
+#25번
+a, b = input().split()
+c = int(a) + int(b)
+print(c)
