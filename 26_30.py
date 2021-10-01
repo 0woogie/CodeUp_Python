612e895614560489e03ab5d8bf7aecb6de13bed5
0woogie <80441481+0woogie@users.noreply.github.com>
2021-10-02 02:33:14 +0900

    Create 26_30.py

---
+++ 26_30.py
@@ -0,0 +1,24 @@
+#26번
+a = input()
+b = input()
+c = float(a) + float(b)
+print(c)
+
+#27번
+a = input()
+n = int(a)
+print('%x' % n)
+
+#28번
+a = input()
+n = int(a)
+print('%X' % n)
+
+#29번
+a = input()
+n = int(a, 16)
+print('%o' % n)
+
+#30번
+n = ord(input())
+print(n)
