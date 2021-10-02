25e560e79cdcb319d3a3f514a33a8a9a0cc045e2
0woogie <80441481+0woogie@users.noreply.github.com>
2021-10-03 01:34:26 +0900

    Create 31_35.py

---
+++ 31_35.py
@@ -0,0 +1,21 @@
+#31번
+c = int(input())
+print(chr(c))
+
+#32번
+n = int(input())
+print(-n)
+
+#33번
+n = ord(input())
+print(chr(n+1))
+
+#34번
+a, b = input().split()
+c = int(a)-int(b)
+print(c)
+
+#35번
+f1, f2 = input().split()
+m = float(f1)*float(f2)
+print(m)
