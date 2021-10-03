b0cfde4acdd0e09b504020c3c92b506d6128ada6
0woogie <80441481+0woogie@users.noreply.github.com>
2021-10-04 01:33:16 +0900

    Create 36_40.py

---
+++ 36_40.py
@@ -0,0 +1,22 @@
+#36번
+w, n = input().split()
+print(w*int(n))
+
+#37번
+n = input()
+s = input()
+print(int(n)*s)
+
+#38번
+a, b = input().split()
+c = int(a)**int(b) 
+print(c)
+
+#39번
+f1, f2 = input().split()
+f3 = float(f1)**float(f2) 
+print(f3)
+
+#40번
+a, b = input().split()
+print(int(a)//int(b))
