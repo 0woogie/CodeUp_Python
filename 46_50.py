f97119699e80709409bf344f9ff485c7e139aa8e
0woogie <80441481+0woogie@users.noreply.github.com>
2021-10-09 00:31:55 +0900

    Update 46_50.py

--- 46_50.py
+++ 46_50.py
@@ -4,8 +4,24 @@ print(n<<1)
 
 #47번
 a, b = input().split()
-print(int(a)<<int(b))
+a = int(a)
+b = int(b)
+print(a<<b)
 
 #48번
 a, b = input().split()
-print(int(a)<int(b))
+a = int(a)
+b = int(b)
+print(a<b)
+
+#49번
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(a==b)
+
+#50번
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(a<=b)
