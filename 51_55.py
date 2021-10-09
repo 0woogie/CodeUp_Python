fc1f49fe8d4d05774e85f5bb5b30b2c2ffc1e8c3
0woogie <80441481+0woogie@users.noreply.github.com>
2021-10-10 00:58:54 +0900

    Create 51_55.py

---
+++ 51_55.py
@@ -0,0 +1,25 @@
+#51번
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(a!=b)
+
+#52번
+n = int(input())
+print(bool(n))
+
+#53번
+n = int(input())
+print(not bool(n))
+
+#54번
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(bool(a) and bool(b))
+
+#55번
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(bool(a) or bool(b))
