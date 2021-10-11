d9b2d194d625fdd548f52a92812bf145d3f115fa
0woogie <80441481+0woogie@users.noreply.github.com>
2021-10-12 01:00:33 +0900

    Update 56_60.py

--- 56_60.py
+++ 56_60.py
@@ -9,3 +9,19 @@ a, b = input().split()
 c = bool(int(a))
 d = bool(int(b))
 print((c and d) or ((not c) and (not d)))
+
+#58번
+a, b = input().split()
+c = bool(int(a))
+d = bool(int(b))
+print((not c) and (not d))
+
+#59번
+a=int(input())
+print(~a)
+
+#60번
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(a & b)
