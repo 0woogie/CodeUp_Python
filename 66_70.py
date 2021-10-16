f39dc033866193a75177cf6da65847b6a843dbcb
0woogie <80441481+0woogie@users.noreply.github.com>
2021-10-16 13:51:38 +0900

    Update 66_70.py

--- 66_70.py
+++ 66_70.py
@@ -20,3 +20,41 @@ else:
     print('C')
   else:
     print('D')
+
+#68번
+n = int(input())
+
+if n>=90:
+    print('A')
+elif n>=70:
+    print('B')
+elif n>=40:
+    print('C')
+else:
+    print('D')
+
+#69번
+c = input()
+
+if c=='A':
+    print("best!!!")
+elif c=='B':
+    print("good!!")
+elif c=='C':
+    print("run!")
+elif c=='D':
+    print("slowly~")
+else:
+    print("what?")
+    
+#70번
+n = int(input())
+
+if n//3==1:
+    print("spring")
+elif n//3==2:
+    print("summer")
+elif n//3==3:
+    print("fall")
+else:
+    print("winter")
