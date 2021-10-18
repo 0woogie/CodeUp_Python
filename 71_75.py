5eec1cb1ba6c63016f3d3d91a55c533794f72b16
0woogie <80441481+0woogie@users.noreply.github.com>
2021-10-18 22:27:21 +0900

    Create 71_75.py

---
+++ 71_75.py
@@ -0,0 +1,20 @@
+#71번
+while True:
+    a = int(input())
+    if a==0:
+        break
+    else:
+        print(a)
+
+#72번
+n = int(input())
+while n!=0:
+    print(n)
+    n -= 1
+    
+#73번
+n = int(input())
+
+while n!=0:
+    n -= 1
+    print(n)
