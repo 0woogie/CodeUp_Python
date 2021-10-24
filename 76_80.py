40b83669c5d2c964b396953fc08e96e4db3da914
0woogie <80441481+0woogie@users.noreply.github.com>
2021-10-25 02:13:38 +0900

    Update 76_80.py

--- 76_80.py
+++ 76_80.py
@@ -4,8 +4,19 @@ for i in range(n+1):
   print(i)
   
 #77번
+n = int(input())
+sum = 0
+for i in range(n+1) :
+  if i%2==0:
+    sum += i
+print(sum)
 
 #78번
+while True:
+     c = input()
+     print(c)
+     if c=='q':
+          break
 
 #79번
 
