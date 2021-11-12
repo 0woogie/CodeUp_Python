30de71cbc62ff37433a91fe147c5a5b3e4540956
0woogie <80441481+0woogie@users.noreply.github.com>
2021-11-12 20:30:23 +0900

    Update 91_95.py

--- 91_95.py
+++ 91_95.py
@@ -19,6 +19,12 @@ for i in range(1, 24):
   print(d[i], end=' ')
 
 #93번
+n = int(input())
+a = input().split()
+for i in range(n):
+  a[i] = int(a[i])
+for i in range(n-1, -1, -1):
+  print(a[i], end=' ')
 
 #94번
 
