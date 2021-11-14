0c36ffb492e8faf2b8ae90e79589aabf74742f58
0woogie <80441481+0woogie@users.noreply.github.com>
2021-11-14 22:30:26 +0900

    Update 91_95.py

--- 91_95.py
+++ 91_95.py
@@ -27,5 +27,18 @@ for i in range(n-1, -1, -1):
   print(a[i], end=' ')
 
 #94번
+n = int(input())
+array = list(map(int, input().split()))
+array.sort()
+print(array[0])
 
 #95번
+d = [[0]*20 for _ in range(20)]
+n = int(input())
+for _ in range(n):
+  x, y = map(int, input().split())
+  d[x][y] = 1
+for i in range(1, 20):
+  for j in range(1, 20): 
+    print(d[i][j], end=' ')
+  print()
